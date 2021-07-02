266d0e3a8c998a45d5635679dc12a3fb7118a576
unknown <anhtuphamxt@gamil.com>
2021-07-02 15:09:09 +0700

    0207

---
+++ src/main/java/com/codegym/Demo0207Application.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0207Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo0207Application.class, args);
+    }
+
+}
